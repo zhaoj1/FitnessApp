17b7a3f4b7f6c488cdaebdb2161c8b5469bd67d7
github username <github email address>
2019-09-30 17:10:35 -0400

    created scraper file

---
+++ scraper.rb
@@ -0,0 +1,56 @@
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+
+def scraper1
+    url = 'https://sworkit.com/exercises'
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    exercises = Array.new 
+    exercise_list = parsed_page.css('div.shadow.card') #452 exercises count (versus #598 as seen on website)
+    exercise_list.each do |exercise_listing|
+        exercise = { #hash of each exercise
+            exercise_name: exercise_listing.css('p').text, #renders name of exercise
+            image_url: exercise_listing.css('img')[1].attributes["src"].value, #renders the exercise image url
+            page_url: "https://sworkit.com" + exercise_listing.css('a')[0].attributes["href"].value
+        }
+        exercises << exercise 
+    end 
+    index = 0 
+    exercises.each do |each_exercise|
+        ex_unparsed_page = HTTParty.get(each_exercise[:page_url])
+        ex_parsed_page = Nokogiri::HTML(ex_unparsed_page)
+        exercise_details = ex_parsed_page.css('div.shadow.card.exercise-card')
+        exercise = {
+            exercise_difficulty: exercise_details.css('p')[0] ? exercise_details.css('p')[0].text : "no information",
+            exercise_impact_level: exercise_details.css('p')[1] ? exercise_details.css('p')[1].text : "no information",
+            target_body_parts: exercise_details.css('p')[2] ? exercise_details.css('p')[2].text : "no information",
+            exercise_video: exercise_details.css('video')[0].attributes['src'].value
+        }
+        exercises[index].merge!(exercise)
+        # Exercise.create(each_exercise)
+        # puts each_exercise
+        puts "creating #{each_exercise[:exercise_name]}"
+        index += 1
+        # byebug
+    end 
+    byebug 
+end 
+
+scraper1 
+
+#then within each individual exercise page i.e.push-ups (We want video,  difficulty, impact level, and target body parts)
+# https://sworkit.com/exercise/push-ups 
+# individual_exercise = parsed_page.css('div.shadow.card.shadow-card')
+# exercise_difficulty = exercise_page.css('p')[0].text
+# exercise_impact_level = exercise_page.css('p')[1].text
+# target_body_parts = exercise_page.css('p')[2].text
+# exercise_video = exercise_page.css('video')[0].attributes['src'].value
+
+
+
+
+
+
+
+
